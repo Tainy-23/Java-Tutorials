e2b5c9672210bcd7a96c7c037b73bc4603c28844
Tainy-23 <111568993+Tainy-23@users.noreply.github.com>
2025-04-12 15:00:59 +0530

    Add of two numbers
    Add 2 ints in and show result.

---
+++ AddOfTwoNumbers.java
@@ -0,0 +1,14 @@
+//Program to perform Arithmetic Operations
+public class AddOfTwoNumbers {
+	
+	public static void main (String[] args) {
+		int a, b, c;
+		a = 10;
+		b = 30;
+		c = a + b;
+		System.out.println("Addition of A and B is " +c);
+		c = a - b;
+		System.out.println("Subtraction of A and B is " +c);
+	}
+}
+
